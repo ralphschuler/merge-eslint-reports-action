e1ea74d3dc1958a3162616f021b0e928d9b73d59
Ralph Schuler <ralph.schuler@nyphon.de>
2023-11-26 01:21:12 +0100

    build

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,20 @@ import * as path from 'path'
  */
 export async function run(): Promise<void> {
   try {
-    const input_directory = core.getInput('input_directory');
-    const output_file = core.getInput('output_file');
+    const input_directory = core.getInput('input_directory')
+    const output_file = core.getInput('output_file')
 
-    const files = fs.readdirSync(input_directory).map((file: string) => path.join(input_directory, file));
+    const files = fs
+      .readdirSync(input_directory)
+      .map((file: string) => path.join(input_directory, file))
     const annotations = files.reduce((acc: any[], file: string) => {
-      const rawReport = fs.readFileSync(file, 'utf8');
-      const parsedReport = JSON.parse(rawReport);
-      return [...acc, ...parsedReport];
-    }, []);
+      const rawReport = fs.readFileSync(file, 'utf8')
+      const parsedReport = JSON.parse(rawReport)
+      return [...acc, ...parsedReport]
+    }, [])
 
-    fs.writeFileSync(output_file, JSON.stringify(annotations, null, 2));
-    core.setOutput('output_file', output_file);
+    fs.writeFileSync(output_file, JSON.stringify(annotations, null, 2))
+    core.setOutput('output_file', output_file)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
